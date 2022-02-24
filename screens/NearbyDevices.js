e1ad7e75fa90dda2feab0f3a20d53c3cf3cb78f9
kebab01 <tjkarbowiak@icloud.com>
2022-02-25 10:27:33 +1100

    Added Neaby devices feature #12

--- screens/NearbyDevices.js
+++ screens/NearbyDevices.js
@@ -8,13 +8,10 @@ import MultiSlider from '@ptomasroos/react-native-multi-slider';
 
 export default function NearbyDevices() {
 
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
   const [errorMsg, setErrorMsg] = useState(null);
-  const [userLocation, setLoaction] = useState(null)
-
-  const [searchRadius, setSearchRadius] = useState(50)
+  const [searchRadius, setSearchRadius] = useState(5)
   const [isLoading, setLoading] = useState(true)
-  console.log(searchRadius)
 
   useEffect(() =>{
 
@@ -25,13 +22,11 @@ export default function NearbyDevices() {
         setErrorMsg('Permission to access location was denied');
         return;
       }
-      const loc = await Location.getCurrentPositionAsync({});
-      setLoaction(loc)
     }
-
-    hasLoaded()
+    
     getData()
-  },[userLocation])
+    hasLoaded()
+  },[])
   
   async function getData(){
     setLoading(true)
@@ -50,15 +45,16 @@ export default function NearbyDevices() {
         dev_with_loc.push(dev)
       }
     }
-    
     getDistances(dev_with_loc)
   }
 
   const getDistances = async(devs) =>{
 
-    if (devs == undefined) return 
-    if (loc == null) return
+    if (devs == undefined) {console.log('returned with no devs');return }
+    // if (userLocation == null) {console.log('returned with no location');return}
+    const userLocation = await Location.getCurrentPositionAsync({});
 
+    console.log('in get distances')
     let devs_dist = devs.map((dev) => {
       
       const lat1 = userLocation?.coords.latitude
@@ -101,15 +97,15 @@ export default function NearbyDevices() {
 
   }
   const renderItem =({item}) =>{
-
+    console.log(item)
     return(
-            <Card>
-                <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', width:'100%', height:30}} onPress={() => handlePress(item)}>
-                    <Text style={globalStyles.text, {flex:1}}>{item.ids.device_id}</Text>
-                    <Text style={globalStyles.text, {flex:1}}>Dist: {item.loc_difference.toFixed(2)}km</Text>
-                    <Image source={require('../assets/arrowBlue.png')} style={{height:20, width:20}}/>
-                </TouchableOpacity>
-            </Card>
+          <Card>
+              <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', width:'100%', height:30}} onPress={() => handlePress(item)}>
+                  <Text style={globalStyles.text, {flex:1}} numberOfLines={1} ellipsizeMode='tail' >{item.ids.device_id}</Text>
+                  <Text style={globalStyles.text, {flex:1, paddingLeft:10}}>Dist: {item.loc_difference.toFixed(2)}km</Text>
+                  <Image source={require('../assets/arrowBlue.png')} style={{height:20, width:20}}/>
+              </TouchableOpacity>
+          </Card>
     )
   }
 
@@ -117,25 +113,26 @@ export default function NearbyDevices() {
     <View style={globalStyles.contentView}>
       <View style={{height:20, margin:20, marginTop:30, justifyContent:'center', alignItems:'center'}}>
         <Text style={{fontWeight:'bold', fontSize:15}}>Search Radius: {searchRadius}km</Text>
-      <MultiSlider
-        value={searchRadius}
-        onValuesChange={(e) => setSearchRadius(e[0])}
-        onValuesChangeFinish={getData}
-        max={301}
-    />
-    </View>
-    {console.log('this is data', data?.length)}
-      {data?.length == 0 || !data ?
-        <FlatList
-        style={{paddingTop:10}}
-        data={data}
-        renderItem={(item) => renderItem(item)}
-        keyExtractor={(item, index) => index.toString()}
-        />
-        :
-        <View style={{alignItems:'center', height:'100%', width:'100%'}}><Text>No devices nearby{console.log('here')}</Text></View>
-      }
-      <LoadingComponent loading={isLoading}/>
+        <MultiSlider
+          value={searchRadius}
+          onValuesChange={(e) => setSearchRadius(e[0])}
+          onValuesChangeFinish={getData}
+          max={301}
+      />
+      </View>
+      <View style={{height:'100%', paddingBottom:150}}>
+        {data.length != 0?
+          <FlatList
+          style={{paddingTop:10}}
+          data={data}
+          renderItem={(item) => renderItem(item)}
+          keyExtractor={(item, index) => index.toString()}
+          />
+          :
+          <View style={{alignItems:'center', height:'100%', width:'100%'}}><Text>No devices nearby</Text></View>
+        }
+        <LoadingComponent loading={isLoading}/>
+      </View>
     </View>
   )
 }
