1248fd6dd29662f8c345ca8a038e4d0f48602545
Thomas Karbowiak <tjkarbowiak@icloud.com>
2022-08-23 14:27:33 +1000

    Added 'extra' key

--- app.config.js
+++ app.config.js
@@ -2,5 +2,9 @@ import 'dotenv/config';
 
 export default({config}) =>{
     config.android.config.googleMaps.apiKey=process.env.GOOGLE_MAPS_API_KEY
+    config.extra={
+        "eas": {
+            "projectId": "c886b8fd-607e-48c2-90a0-302f2e6dfd94"
+        }}
     return config
 }
